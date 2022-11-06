df5e78a9e61a18b22b6931d74fa38d0a3304f37e
nur-sh <nsheikhmohammed01@gmail.com>
2022-11-06 17:44:27 -0500

    Add code for labels and status

--- code/rqc/src/components/IssuesList.jsx
+++ code/rqc/src/components/IssuesList.jsx
@@ -2,9 +2,13 @@ import { useQuery } from "react-query";
 import { IssueItem } from "./IssueItem";
 
 
-export default function IssuesList() {
-  const issuesQuery = useQuery(["issues"], () =>
-    fetch("/api/issues").then((res) => res.json())
+export default function IssuesList({ labels, status }) {
+  const issuesQuery = useQuery(["issues", { labels, status }], () => 
+    {
+      const statusString = status ? `&status=${status}` : "";
+      const labelsString = labels.map((label => `labels[]=${label}`)).join("&");
+      return fetch(`/api/issues?${labelsString}${statusString}`).then((res) => res.json());
+    }
   );
 
   return (
