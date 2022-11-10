983c32869333e79eb3a8a97db4180352ef0bad94
nur-sh <nsheikhmohammed01@gmail.com>
2022-11-10 01:28:53 -0500

    Add search field to issue tracker

---
+++ code/rqc/src/components/IssueHeader.jsx
@@ -0,0 +1,29 @@
+import { GoIssueClosed, GoIssueOpened } from "react-icons/go";
+import { possibleStatus } from '../helpers/defaultData';
+import { relativeDate } from "../helpers/relativeDate";
+import { useUserData } from "../helpers/useUserData";
+
+export const IssueHeader = ({
+  title, number, status = "todo", createdBy, createdDate, comments
+}) => {
+  const statusObject = possibleStatus.find((pstatus) => pstatus.id === status);
+  const createdUser = useUserData(createdBy);
+
+  return (
+    <header>
+      <h2>{title} <span>#{number}</span></h2>
+      <div>
+        <span className={status === "done" || status === "cancelled" ? "closed" : "open"}>
+          {status === "done" || status === "cancelled"
+            ? <GoIssueClosed />
+            : <GoIssueOpened />}
+          {statusObject.label}
+        </span>
+        <span>
+          {createdUser.isLoading ? "..." : createdUser.data?.name}
+        </span>{" "}
+        opened this issue {`${relativeDate(createdDate)} \u00B7 ${comments.length}`}{" "}
+      </div>
+    </header>
+  );
+};
