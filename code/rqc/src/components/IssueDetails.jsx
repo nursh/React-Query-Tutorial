690bde40da56d5f864a8e267b54f3f0b0610d8ff
nur-sh <nsheikhmohammed01@gmail.com>
2023-01-09 01:05:25 -0500

    Add Issue Assignment to Issue page.

--- code/rqc/src/components/IssueDetails.jsx
+++ code/rqc/src/components/IssueDetails.jsx
@@ -4,6 +4,7 @@ import { relativeDate } from "../helpers/relativeDate";
 import { IssueHeader } from "./IssueHeader";
 import { useUserData } from "../helpers/useUserData";
 import IssueStatus from "./IssueStatus";
+import IssueAssignment from "./IssueAssignment";
 
 function useIssueData(issueNumber) {
   return useQuery(["issues", issueNumber], ({ signal }) => {
@@ -43,6 +44,7 @@ export default function IssueDetails() {
             </section>
             <aside>
               <IssueStatus status={issueQuery.data.status} issueNumber={issueQuery.data.number.toString()} />
+              <IssueAssignment assignee={issueQuery.data.assignee} issueNumber={issueQuery.data.number.toString()} />
             </aside>
           </main>
         </>
